e503f1a07e3003de070ffd63b6f97ef0731d9286
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-07-04 17:27:32 -0500

    Numbers and Arithmetic
    created simple arithmetic using Python. In order to print the results, I had to convert the equation into a string. This was done by appending "str" to the variable name that performs the equation.

---
+++ numbers.py
@@ -0,0 +1,25 @@
+print("Performing Math Equations in Python");
+
+addition = 2+1;
+addition_string = str(addition);
+print("Addition: 2 + 1 = "+ addition_string);
+
+subtraction = 43 - 12;
+subtraction_string = str(subtraction);
+print("Subtraction: 43 - 12 = " + subtraction_string);
+
+multiplication = 9 * 45;
+multi_string = str(multiplication);
+print("Multiplication: 9 * 45 = " + multi_string);
+
+division = 300 / 25;
+division_string = str(division);
+print("Division: 300 / 25 = " + division_string);
+
+modulo = 5 % 2;
+modulo_string = str(modulo);
+print("Modulo: 5 % 2 = " + modulo_string);
+
+power = 8 ** 4;
+power_string = str(power);
+print("Power: 8 ** 4 = " + power_string);
