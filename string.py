138457142f8d36c4da0faac0a991bda22d51252d
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-07-06 15:19:51 -0500

    Python String
    working with Python strings and using escape keys

---
+++ string.py
@@ -0,0 +1,10 @@
+print("Working with Python Strings");
+
+#using \n to print on a new line
+print("1st line printed. \nPrinting on a new line!")
+
+#using \t to tab over within the line
+print("Please give me some \tspace!");
+
+#printing the length of the string
+print(len("My name is Carleena"));
