4c2ab6a6d42bfe09477e5b64cbcfa899db9a0407
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-07-18 15:27:41 -0500

    Strings Properties and Methods
    learned about built in python methods and learned how to slice a string.

---
+++ stringPropertiesAndMethods.py
@@ -0,0 +1,20 @@
+#Goal is to update the name "Sam" to "Pam" using slicing.
+name = "Sam"
+last_letters = name[1:]
+print ("P"+last_letters)
+
+#sting multiplication
+x = 'Repeating Myself '
+print(x * 10)
+
+#using different Methods in Python
+#capitalize only capitalizes the first letter in a string while the upper capitalizes the entire string
+y = 'hello world'
+print(y.upper())
+
+#split string into a list
+h = "Hello everyone this is a string"
+#splits on the white space
+print(h.split())
+#splits on the letter e
+print(h.split('e'))
