4735b063b79dbcbed179cc5fc03560e079dfa4fd
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-07-22 20:19:03 -0500

    Tuples in Python
    learned how to use tuples

---
+++ tuplesInPython.py
@@ -0,0 +1,14 @@
+#tuples are immutable
+t = (1,2,3)
+print (type(t))
+print (len(t))
+
+#grabbing index of tuple
+x = (1,'two',3)
+print (x[1])
+print(x[-1])
+
+#count and index in tuple
+z = ('a','c','f','c','o','c')
+print (z.count('c'))
+print(z.index('o'))
