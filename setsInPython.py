b1d0e77e25d7c8114a38599abd7a850ee0d66f7c
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-07-23 13:24:03 -0500

    Sets in Python
    Learned about sets in Python

---
+++ setsInPython.py
@@ -0,0 +1,8 @@
+#set is an unordered collection of unique elements
+mySet = set()
+mySet.add(2)
+print(mySet)
+mySet.add(4)
+print(mySet)
+newList = [1,1,1,2,2,2,3,33,6,66,4,4,44,9,9]
+print(set(newList))
